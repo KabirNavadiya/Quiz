0c2caada0076376388b33d58a440881612e5064b
KabirNavadiya <kabirnavadia27@gmail.com>
2025-02-10 19:12:38 +0530

    TE-T127 PR-57 : Quiz task - added restart button, also added validation to input.

--- script/script.js
+++ script/script.js
@@ -2,6 +2,9 @@ let questions = [];
 let cindex = 0;
 let score = 0;
 $("#submitAnswer").hide();
+$("#restart").hide();
+
+
 $.ajax({
     type: "GET",
     url: "script/quiz.json",
@@ -9,18 +12,28 @@ $.ajax({
 })
 .done(function (data) {
     questions = data;
-    console.log(questions);
+    // console.log(questions);
     // displayQuestion(cindex);
 })
 
 
+
 function displayQuestion(indx) {
     $("#quiz").empty();
+    $("#submitAnswer").show()
+    $("#submitAnswer").text("Next");
+    $("#restart").hide()
     let currentQuestion = questions[indx];
     console.log(questions.length,cindex);
     if(cindex >= questions.length){
         alert(`score : ${score}`);
+        $("#getQuestion").hide();
+        $("#submitAnswer").hide();
+        $("#restart").show();
         return ;
+    }
+    if(cindex === questions.length - 1 ){
+        $("#submitAnswer").text("Submit");
     }
 
     $("#quiz").append(
@@ -47,8 +60,13 @@ $(document).ready(function () {
 
 $(document).ready(function () {
     $("#submitAnswer").click(function(){
-        console.log("submit");
-        let userInput = document.querySelector("input[name = answer]:checked").value;
+
+        // console.log("submit");
+        let userInput = $("input[name = answer]:checked").val();
+        if (!userInput) {
+            alert("Please select an answer before submitting!");
+            return;
+        }
         // console.log(userInput);
         if(userInput === questions[cindex].answer){
             // console.log("correct answer");
@@ -57,4 +75,13 @@ $(document).ready(function () {
         cindex++;
         displayQuestion(cindex); 
     })
+});
+
+$(document).ready(function () {
+    $("#restart").click(function () { 
+        $("#getQuestion").show();
+        cindex=0;
+        score=0;
+        displayQuestion(cindex);
+    });
 });
