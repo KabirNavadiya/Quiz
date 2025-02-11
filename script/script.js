3eb25cc166944847325b055c990b5bcaf2c86583
KabirNavadiya <kabirnavadia27@gmail.com>
2025-02-11 12:01:58 +0530

    TE-T127 PR-57 Jquery-AJAX : added Styles to Quiz task.

--- script/script.js
+++ script/script.js
@@ -3,36 +3,32 @@ let cindex = 0;
 let score = 0;
 $("#submitAnswer").hide();
 $("#restart").hide();
-
+$("#quiz").hide();
 
 $.ajax({
     type: "GET",
     url: "script/quiz.json",
     dataType: "json",
-})
-.done(function (data) {
+}).done(function (data) {
     questions = data;
-    // console.log(questions);
-    // displayQuestion(cindex);
 })
-
-
 
 function displayQuestion(indx) {
     $("#quiz").empty();
+    $("#quiz").fadeIn();
     $("#submitAnswer").show()
     $("#submitAnswer").text("Next");
-    $("#restart").hide()
+
+    $("#getQuestion").hide();
     let currentQuestion = questions[indx];
-    console.log(questions.length,cindex);
-    if(cindex >= questions.length){
+    if (cindex >= questions.length) {
         alert(`score : ${score}`);
-        $("#getQuestion").hide();
-        $("#submitAnswer").hide();
         $("#restart").show();
-        return ;
+        $("#quiz").hide();
+        $("#submitAnswer").hide();
+        return;
     }
-    if(cindex === questions.length - 1 ){
+    if (cindex === questions.length - 1) {
         $("#submitAnswer").text("Submit");
     }
 
@@ -40,48 +36,46 @@ function displayQuestion(indx) {
         $("<h4>").text(`${indx + 1}. ${currentQuestion.question}`),
         $("<ul>").append(
             currentQuestion.options.map(option => {
-                // console.log(option);
                 return $("<li>").append(
-                    $("<input>").attr({ type: "radio", name: "answer", value: option }),
-                    $("<span>").text(option)
+                    $("<label>").addClass("option-label").append(
+                        $("<input>").attr({ type: "radio", name: "answer", value: option }).addClass("form-input"),
+                        $("<span>").text(option)
+                    )
                 );
             })
         ),
-    )       
-   
+    )
+
 }
 $(document).ready(function () {
     $("#getQuestion").click(function () {
-        displayQuestion(cindex);  
+
+        displayQuestion(cindex);
         $("#submitAnswer").show()
-   
+
     });
 });
 
 $(document).ready(function () {
-    $("#submitAnswer").click(function(){
-
-        // console.log("submit");
+    $("#submitAnswer").click(function () {
         let userInput = $("input[name = answer]:checked").val();
         if (!userInput) {
             alert("Please select an answer before submitting!");
             return;
         }
-        // console.log(userInput);
-        if(userInput === questions[cindex].answer){
-            // console.log("correct answer");
+        if (userInput === questions[cindex].answer) {
             score++;
         }
         cindex++;
-        displayQuestion(cindex); 
+        displayQuestion(cindex);
     })
 });
 
 $(document).ready(function () {
-    $("#restart").click(function () { 
-        $("#getQuestion").show();
-        cindex=0;
-        score=0;
+    $("#restart").click(function () {
+        $("#restart").hide();
+        cindex = 0;
+        score = 0;
         displayQuestion(cindex);
     });
 });
